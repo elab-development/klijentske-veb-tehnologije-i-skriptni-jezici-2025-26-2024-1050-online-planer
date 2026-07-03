3b547476b0be2adf659809889dff7bc4e58053b9
talayya <at20241050@student.fon.bg.ac.rs>
2026-07-03 22:44:00 +0200

    User type & predefined users

---
+++ src/data/users.ts
@@ -0,0 +1,22 @@
+import type { User } from '../types/User';
+
+export const predefinedUsers: User[] = [
+  {
+    id: 1,
+    name: 'Nikola Petrović',
+    email: 'nikola@planit.com',
+    password: 'nikola123',
+  },
+  {
+    id: 2,
+    name: 'Mina Jovanović',
+    email: 'mina@planit.com',
+    password: 'mina123',
+  },
+  {
+    id: 3,
+    name: 'Marko Ilić',
+    email: 'marko@planit.com',
+    password: 'marko123',
+  },
+];
